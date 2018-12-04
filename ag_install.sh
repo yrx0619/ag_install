9a8ac399895f0d7d18a6c81783bc62190afa5950
yerx <yrx0619@126.com>
2018-12-05 01:45:45 +0800

    update| jugement os type.

--- ag_install.sh
+++ ag_install.sh
@@ -1,14 +1,20 @@
-sudo yum install -y xz-devel zlib-devel pcre-devel
+os="`python -mplatform | grep Ubuntu`"
+if [ $os ]
+then
+    sudo apt-get install automake liblzma-dev
+else
+    sudo yum install -y xz-devel zlib-devel pcre-devel
+fi
 
-wget https://github.com/ggreer/the_silver_searcher/archive/master.zip 
+wget https://github.com/ggreer/the_silver_searcher/archive/master.zip
 
-mv master.zip ag.zip 
+mv master.zip ag.zip
 
-unzip ag.zip 
+unzip ag.zip
 
-cd the_silver_searcher-master/ 
+cd the_silver_searcher-master/
 
-./build.sh 
+./build.sh
 
 sudo make install
 
