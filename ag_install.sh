ea99c84badd4e142b79bf624d938c23a577205c8
Luffy.AN <1056432250@qq.com>
2019-10-29 17:38:55 +0800

    Update ag_install.sh

--- ag_install.sh
+++ ag_install.sh
@@ -1,22 +1,22 @@
 os="`python -mplatform | grep Ubuntu`"
 if [ $os ]
 then
-    sudo apt-get install automake liblzma-dev
+    sudo apt-get install automake liblzma-dev || exit 1
 else
-    sudo yum install -y xz-devel zlib-devel pcre-devel
+    sudo yum install -y xz-devel zlib-devel pcre-devel || exit 1
 fi
 
-wget https://github.com/ggreer/the_silver_searcher/archive/master.zip
+wget https://github.com/ggreer/the_silver_searcher/archive/master.zip || exit 1
 
-mv master.zip ag.zip
+mv master.zip ag.zip || exit 1
 
-unzip ag.zip
+unzip ag.zip || exit 1
 
-cd the_silver_searcher-master/
+cd the_silver_searcher-master/ || exit 1
 
-./build.sh
+./build.sh || exit 1
 
-sudo make install
+sudo make install || exit 1
 
-ag --version
+ag --version || exit 1
 
